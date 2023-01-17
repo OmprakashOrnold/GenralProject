1e975dbb453b6a1c13ae3fbee30701982565961d
OmPrakashpDev <112843258+OmPrakashpDev@users.noreply.github.com>
2023-01-17 07:55:25 +0530

    first commit

---
+++ src/main/java/com/gp/GeneralProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralProjectApplication.class, args);
+    }
+
+}
